7c6f712615df27d91fbdfbdbe6746030d55407bd
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-02-18 11:26:26 +0530

    18-2-2024

---
+++ ClassRoom/Class 4 Conditionals 2/2.cpp
@@ -0,0 +1,7 @@
+#include <iostream> 
+using namespace std;
+int main(){
+    cout<<"Enter your marks: ";
+    int n;cin>>n;
+    cout<<(n>=33 ?  "Passed!" : "Failed!");
+}
---
+++ ClassRoom/Class 4 Conditionals 2/3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Enter a number between 1 and 7 to print the day!";
+    int n;cin>>n;
+    switch(n){
+        case 1:cout<<"Monday";break;
+        case 2:cout<<"Tuesday";break;
+        case 3:cout<<"Wednesday";break;
+        case 4:cout<<"Thursday";break;
+        case 5:cout<<"Friday";break;
+        case 6:cout<<"Saturday";break;
+        case 7:cout<<"Sunday";break;
+        default:cout<<"Wrong Input!";
+    }
+}

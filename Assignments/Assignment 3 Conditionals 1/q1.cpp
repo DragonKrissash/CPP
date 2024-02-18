cb36b1f274a63b1c69154e2efcf72df93b7b6694
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-02-18 10:26:44 +0530

    18-2-2024

---
+++ Assignments/Assignment 3 Conditionals 1/q1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Enter 2 numbers: ";
+    int a,b;cin>>a>>b;
+    if(a>b)cout<<"The Larger number is: "<<a;
+    else if(b>a)cout<<"The Larger number is "<<b;
+    else cout<<"Both are equal!";
+
+}

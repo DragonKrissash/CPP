dcdb8ae0233a232998968024b9e86a2e435336ae
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-02-18 09:33:06 +0530

    18-2-2024

---
+++ Assignments/Assignment 2 Input and Operators/q2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;cout<<"Enter the first number: ";
+    cin>>x;int y;cout<<"Enter the second number: ";
+    cin>>y;cout<<(x!=y)<<" "<<(x>=y);
+    
+}

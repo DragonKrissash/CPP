396648fafc298e3aac13776054471a0dfeb78786
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-02-18 09:53:31 +0530

    18-2-2024

---
+++ ClassRoom/Class 3 Conditionals 1/2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Enter a number: ";
+    int n;cin>>n;
+    if(n>=0)cout<<"The absolute value of the number is: "<<n;
+    else cout<<"The absolute value of the number is: "<<-n;
+}

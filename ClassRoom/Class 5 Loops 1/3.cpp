ee8bad7c1459f93cdccea783aa34ad1621e5b632
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-02-18 11:55:12 +0530

    18-2-2024

---
+++ ClassRoom/Class 5 Loops 1/3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Enter the number upto which the series should be printed: ";
+    int n;cin>>n;
+    for (int a=1;a<=n;a+=2)
+    cout<<a<<" 5";
+}

79835e048a5064f151efcf948d97c548d15b661e
DragonKrissash <dragonkrishoredbzgt@gmail.com>
2024-02-24 19:31:49 +0530

    24-2-2024

---
+++ Assignments/Assignment 14 2d Arrays 1/q1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<vector<int>>v(5,vector<int>(5,10));
+    for(int a=0;a<5;a++)
+    {
+        for(int b=0;b<5;b++)
+        cout<<v[a][b]<<" ";
+        cout<<endl;
+    }
+}
